76d33208f62fd230184db69fcdfd5fa0409cba90
Aleksandr F <a7214370@gmail.com>
2022-06-24 21:13:40 +0300

    Setup ViewController

--- TConverter/ViewController.swift
+++ TConverter/ViewController.swift
@@ -11,7 +11,13 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var celsiusLabel: UILabel!
     @IBOutlet weak var fahrenheitLabel: UILabel!
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
+    }
     
 
     @IBAction func sliderChanged(_ sender: UISlider) {
